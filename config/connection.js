60ebe90f8ce50bd61bc57c4ae721f353dd25d471
Jose Hernandez Torres <jose@hernandezt.com>
2019-02-11 17:51:54 -0700

    config updated

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,19 @@
 const mysql = require("mysql");
 let connection;
 
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "root1234",
-        database: "burgers_db"
+            host: "localhost",
+            port: 3306,
+            user: "root",
+            password: "root1234",
+            database: "burgers_db"
+    });
 }
-
-
-});
-
 connection.connect((err) => {
-    if(err){
+    if (err) {
         console.log("Error connecting: " + err.stack);
         return;
     }
